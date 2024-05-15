5bf0106900902b5a7a8652de8645ecaca86d82ab
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-05-16 00:13:58 +0530

    Update reverseString() to use cascade functions approach

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,18 +1,5 @@
 const reverseString = function(inputString) {
-
-    let reversedString = "";
-
-    let charAtIndex = '';
-
-    for(let i = 0; i < inputString.length; i++) {
-        
-        charAtIndex = inputString.charAt(i);
-
-        reversedString = charAtIndex + reversedString;
-
-    }
-    
-    return reversedString;
+    return inputString.split("").reverse().join("");
 
 };
 
