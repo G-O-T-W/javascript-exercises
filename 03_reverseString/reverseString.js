ec882412b504f0dd27ba1b1ed1d75186c2028051
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-05-16 00:04:59 +0530

    Update reverseString() function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let reversedString = "";
+
+    let charAtIndex = '';
+
+    for(let i = 0; i < inputString.length; i++) {
+        
+        charAtIndex = inputString.charAt(i);
+
+        reversedString = charAtIndex + reversedString;
+
+    }
+    
+    return reversedString;
 
 };
 
