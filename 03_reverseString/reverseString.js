c17a6f2d4dac1304747266c1f46b34fafa6ef6fa
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-10 00:08:16 +0530

    Pass Test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+    let reversedString = "";
+    for (ch of str) {
+        reversedString = ch + reversedString;
+    }
+    return reversedString;
+    }
 
 // Do not edit below this line
 module.exports = reverseString;
