e05c13a10d1249e078013d27338777a469e861b7
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-10 15:45:03 +0530

    Pass Test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) { 
+  return Math.round((fah - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  fah = (9.0 / 5) * cel + 32;
+  if (Math.round(fah) == fah) {
+    return Math.round(fah);
+  } else {
+    return Number.parseFloat(fah.toFixed(1));
+  }
 };
 
 // Do not edit below this line
