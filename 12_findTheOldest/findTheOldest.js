05894b0fcb0173ff32e79a624c143b2367df12e5
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-15 01:54:31 +0530

    Pass Test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sortedPeople = people.slice();
+    sortedPeople.forEach(a => {
+        console.log(a.yearOfBirth);
+    });
+    sortedPeople.sort((a, b) => {
+    let ageOfA = ((a.yearOfDeath)? a.yearOfDeath : (new Date()).getFullYear()) - a.yearOfBirth; 
+    let ageOfB = ((b.yearOfDeath)? b.yearOfDeath : (new Date()).getFullYear()) - b.yearOfBirth;
+    if (ageOfA > ageOfB) {
+        return -1;
+    } else {
+        return 1;
+    }
+  });
+  console.log(sortedPeople);
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
