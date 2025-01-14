b03c30dd38d363cb7732802bbf015fd439a9e4ea
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-14 23:36:48 +0530

    Pass Test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const regExp = /[a-zA-Z0-9]/;
+  str = str.toLowerCase();
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    let ch = str.charAt(i);
+    arr.push(ch);
+  }
+  arr = arr.filter(item => regExp.test(item));
 
+  if ( arr.join('') === arr.reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
