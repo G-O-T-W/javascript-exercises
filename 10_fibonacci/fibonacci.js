a31e1ad6cd953f5be3b3d9d954fbd70715f72e11
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-14 23:50:36 +0530

    Pass Test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  index = parseInt(index);
+  let t1 = 1;
+  let t2 = 1;
+  let sum = 0;
+  if (index == 0) {
+    return 0;
+  } else if (index < 0) {
+    return "OOPS";
+  } else if (index == 1 || index == 2) {
+    return t1;
+  } else {
+    for (let i = 3; i <= index; i++) {
+        sum = t1 + t2;
+        t1 = t2;
+        t2 = sum;
+        if (i === index) {
+            return sum;
+        }
+    }
+  } 
 };
 
 // Do not edit below this line
